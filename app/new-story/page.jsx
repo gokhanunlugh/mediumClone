9d21cfa0c1a8689a9a6864e94b864ae4da6b9855
Gokhan <gokhanunlu94@gmail.com>
2024-10-15 16:18:47 +0300

    new-story sayfası oluşturuldu

---
+++ app/new-story/page.jsx
@@ -0,0 +1,11 @@
+export default function NewStory(){
+
+  return (
+    <div className="draft">
+      <form action="">
+      <input type="text" name="title" placeholder="Title"/>
+      <input type="text" name="content" placeholder="Tell your story..." />
+      </form>
+    </div>
+  )
+}
