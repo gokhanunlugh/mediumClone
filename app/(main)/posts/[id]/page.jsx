0a9d0b29c8ecc7a714ed15dfa35217dd3518edb1
Gokhan <gokhanunlu94@gmail.com>
2024-10-15 16:57:33 +0300

    dosyalar maine tasındı. post id slug oluşturuldu

---
+++ app/(main)/posts/[id]/page.jsx
@@ -0,0 +1,19 @@
+import { createClient } from "@/utils/supabase/server";
+import { notFound } from "next/navigation";
+
+
+
+export default async function PostDetailsPage({params}){
+  const supabase = createClient();
+  const {data, error} = await supabase.from('posts').select().eq('id', params.id).single();
+
+  if(!data) return notFound();
+
+  return (
+    <div className="post">
+      <h1>{data.title}</h1>
+      <p>{data.content}</p>
+    </div>
+  )
+
+}
