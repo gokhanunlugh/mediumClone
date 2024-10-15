dc2324553ad7503775e9208d83aadd47cfd0fea3
Gokhan <gokhanunlu94@gmail.com>
2024-10-15 16:45:52 +0300

    use pathname eklendi

--- components/header/page.jsx
+++ components/header/page.jsx
@@ -1,7 +1,13 @@
+"use client"
 import Link from "next/link"
+import { usePathname  } from "next/navigation"
 
 
 export default function Header() {
+  const pathname = usePathname();
+  
+  
+  
 
   return (
     <header>
@@ -10,7 +16,7 @@ export default function Header() {
         <input type="text" name="search" id="" />
       </div>
       <div className="user">
-        <Link href={'/new-story'}>Write</Link>
+        {pathname === '/new-story' ? <button type="submit" form="draft">Publish</button> : <Link href={'/new-story'}>Write</Link>}
       </div>
     </header>
   )
