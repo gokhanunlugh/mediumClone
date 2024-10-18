274756df95ba633f72b71524e0bdb85967437806
Gokhan <gokhanunlu94@gmail.com>
2024-10-18 23:56:12 +0300

    login css

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -2,14 +2,14 @@ import { login, signOut, signup } from './actions'
 
 export default function LoginPage() {
   return (
-    <form>
-      <label htmlFor="email">Email:</label>
+    <form className='login'>
+      <h4>Sign in with email</h4>
+      <p>Enter the email address associated with your account, and we’ll send a magic link to your inbox.</p>
+      <label htmlFor="email">Your email</label>
       <input id="email" name="email" type="email" required />
-      <label htmlFor="password">Password:</label>
+      <label htmlFor="password">Your password</label>
       <input id="password" name="password" type="password" required />
-      <button formAction={login}>Log in</button>
-      <button formAction={signup}>Sign up</button>
-      <button formAction={signOut}>Sign Out</button>
+      <button formAction={login}>Continue</button>
     </form>
   )
 }
